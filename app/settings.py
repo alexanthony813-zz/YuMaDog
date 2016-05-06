cbdff56eea22864d80084bd9cb8627b6afa7a5db
Alex Anthony <alexanderanthony813@gmail.com>
2016-05-05 20:14:09 -0700

    Added whitenoise

--- app/settings.py
+++ app/settings.py
@@ -14,6 +14,17 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
 
 
 # Quick-start development settings - unsuitable for production
