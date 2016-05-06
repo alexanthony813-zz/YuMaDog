64cd94c11a109ec0f8e0771ca48cf8d324ea2064
Alex Anthony <alexanderanthony813@gmail.com>
2016-05-05 21:11:44 -0700

    Modified git ignore

---
+++ config.py
@@ -0,0 +1,4 @@
+#Production Variables
+DB_SECRET = '!dosox_-$x_)w*2gk-wcg)vpv0oi^d5pp*xjf9w(i&sf!^ch_v'
+API_KEY = 'e941c283e7da908741b97cf198cef9a8'
+API_SECRET = 'f6a0798b633dc9f6b34d14afa3de0d3e'
