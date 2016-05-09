fa8133755e754627d61612f8cb4ebee0b2edd390
Alex Anthony <alexanderanthony813@gmail.com>
2016-05-09 13:20:45 -0700

    Logo now added to navbar

--- assets/js/Nav.js
+++ assets/js/Nav.js
@@ -5,7 +5,10 @@ var Search = require('./Search');
 module.exports= function(props){
   return (
     <nav className="navbar">
-      <div className="col-md-6 col-md-offset-3">
+      <span id="logo">
+        <span id="yuma">Yuma</span><div id="dog"><span> Dog </span></div>
+      </span>
+      <div className="col-md-6 col-md-offset-2">
         <Search searchHandler={props.searchHandler}/>
       </div>
     </nav>
